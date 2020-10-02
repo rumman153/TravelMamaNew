a6fb8a705a18065b401acafd00cf197789a8d95d
Rumman Ahmed Prodhan <rumman153@gmail.com>
2020-10-02 16:32:06 +0600

    cascade

--- ManageVisitor/models.py
+++ ManageVisitor/models.py
@@ -6,10 +6,14 @@ from django.contrib.auth.models import User
 class Visitor(models.Model):
     Visitor_name = models.CharField(max_length=100)
     Visitor_email = models.EmailField(max_length=100,unique=True)
-    Gender = models.CharField(max_length=50)
+    Gender = models.CharField(max_length=50,
+                                     choices=(('Male', 'Male'),
+                                              ('Female', 'Female'),
+                                              ),
+                                     default='Male')
     Visitor_picture = models.ImageField(upload_to='images/Visitor_profile/', default=1)
 
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.Visitor_name
